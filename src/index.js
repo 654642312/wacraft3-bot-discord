7d6b0710249f16c1310c8a929879ee643e69c34d
Edgar018 <Edgar_belandria010@hotmail.com>
2020-09-11 17:26:14 +0200

    agregado un comando para ver contra quien esta jugando ahora mismo

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const puppeteer = require('puppeteer');
 const { Client } = require('discord.js');
 const { prefix } = require('./config.json');
 
-const initBrowser = async (player, message) => {
+const showStats = async (player, message) => {
 
   let statsList = [];
   let raceList = [];
@@ -29,18 +29,41 @@ const initBrowser = async (player, message) => {
     let playerStats = $(element).text();
     statsList.push(playerStats);
   });
+
   const image = $('div.v-card__text div div div div div div').html();
 
-  console.log(image)
+  const elemetHTML = image.split(' ')[8]
+  const imageClip = elemetHTML.substring(10,33)
+  const imagePlayer = imageClip.split('.').slice(0, -1).join('.')
+
+  const playerAvatar = 'https://www.w3champions.com' + imagePlayer + '.jpg';
 
   browser.close();
 
   for(let i = 0; i<raceList.length; i++){
-    message.channel.send(raceList[i][2].substring(6))
+    message.channel.send(raceList[i][2].substring(6));
     message.channel.send(statsList[i]);
   }
- 
+  message.channel.send(playerAvatar);
+  
 };
+
+const showOponent = async (player, message) => {
+  const browser = await puppeteer.launch({headless: false});
+  const page = await browser.newPage();
+  await page.goto(`https://www.w3champions.com/player/${player}`,{
+    timeout: 0
+  });
+  const content = await page.content();
+  const $ = cheerio.load(content);
+
+  const liveMatch = $('div.live-match__container div').next().text();
+
+  browser.close();
+
+  message.channel.send(liveMatch.split(' ')[1] + ' VS ' + liveMatch.split(' ')[3]);
+  message.channel.send(liveMatch.split(' ')[5] + liveMatch.split(' ')[6]);
+}
 
 
 const client = new Client();
@@ -52,7 +75,14 @@ client.on('ready', () => {
 client.on('message', (message) => {
   if(message.content.startsWith(prefix + 'profile ')){
     const player = message.content.split(' ')[1];
-    initBrowser(player, message);
+    showStats(player, message);
+  }
+});
+
+client.on('message', (message) => {
+  if(message.content.startsWith(prefix + 'PlayNow ')){
+    const player = message.content.split(' ')[1];
+    showOponent(player, message)
   }
 });
 
