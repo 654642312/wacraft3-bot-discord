425cf1864557141569357ca77fb7c3549624b435
Edgar018 <Edgar_belandria010@hotmail.com>
2020-09-28 19:32:52 -0400

    no mostrar jugadores sin partidas

--- src/libs/embed.js
+++ src/libs/embed.js
@@ -5,13 +5,15 @@ const makeEmbed = (embed, name, playerAvatar, raceList, statsList, message) => {
     .setThumbnail(playerAvatar)
     .setImage(playerAvatar);
     
-
-    for(let i = 0; i<raceList.length; i++){
-        embed.addFields(
-            { name: raceList[i][2].substring(6).replace(/['"]+/g, ''), value: statsList[i] }
-        );
+    if(statsList.length !== 0){
+        for(let i = 0; i<raceList.length; i++){
+            embed.addFields(
+                { name: raceList[i][2].substring(6).replace(/['"]+/g, ''), value: statsList[i] }
+            );
+        }
+        return message.channel.send(embed);
     }
-    message.channel.send(embed);
+    message.channel.send('This player has no games');
 }
 
 module.exports = makeEmbed
